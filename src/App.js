fe2e6036054cc3d4859b5bc9d5a43f4162ef0c41
Maxim Bureev <mvbureev@gmail.com>
2021-10-14 17:56:34 +0400

    feat: CRUD for todos localStorage

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
 import 'antd/dist/antd.css';
 
 import { Divider } from 'antd';
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewTodoForm from './components/NewTodoForm';
 import TodoList from './components/TodoList';
-
-const listMock = [
-  {
-    title: 'Task 1',
-    checked: false,
-  },
-  {
-    title: 'Task 2',
-    checked: true,
-  },
-  {
-    title: 'Task 3',
-    checked: false,
-  },
-];
+import * as helpers from './helpers';
 
 function App() {
+  const [todosLS, setTodosLS] = useState(helpers.getTodoItemsFromLocalStorage('todo') || []);
+  const save = (data) => {
+    setTodosLS(data);
+    helpers.saveTodoItemsToLocalStorage('todo', data);
+  };
+
+  const createNewTodo = (title) => {
+    const id = helpers.genId();
+    save([...todosLS, {
+      id,
+      title,
+      checked: false,
+    }]);
+  };
+
+  const updateTodo = (id, value, key) => {
+    save([...todosLS.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          [key]: value,
+        };
+      }
+      return todo;
+    })]);
+  };
+
+  const deleteTodo = (id) => save([...todosLS.filter((todo) => todo.id !== id)]);
+
+  const getActions = (id) => ({
+    onChange: (title) => updateTodo(id, title, 'title'),
+    onCheckedChange: (checked) => updateTodo(id, checked, 'checked'),
+    onRemove: () => deleteTodo(id),
+  });
+
+  const todoItems = todosLS.map((todo) => ({
+    ...todo,
+    ...getActions(todo.id),
+  }));
+
   return (
     <div className="App">
       <div className="todos">
-        <NewTodoForm onSubmit={(e) => console.log(e)} />
+        <NewTodoForm onSubmit={createNewTodo} />
         <Divider />
-        <TodoList list={listMock} />
+        <TodoList list={todoItems} />
       </div>
     </div>
   );
---
+++ src/helpers.js
@@ -0,0 +1,28 @@
+export const getTodoItemsFromLocalStorage = (key) => {
+  const value = localStorage.getItem(key); // Get the JSON string stored with provided key in localStorage
+
+  let todoItems = null; // Initialize todoItems variable to be assigned a final value later on
+
+  // Using try catch to avoiding exception to be thrown if the JSON string is invalid
+  // It may be a normal string and we can't JSON.parse it
+  try {
+    const parsedJSON = JSON.parse(value);
+
+    if (Array.isArray(parsedJSON)) {
+      todoItems = parsedJSON;
+    }
+  } catch (e) {
+    // If it's not a valid JSON string, then we should initialize an empty array for todoItems
+    todoItems = [];
+  }
+
+  return todoItems;
+};
+
+// This one is quite straightforward, it better have a comment too :))
+export const saveTodoItemsToLocalStorage = (key, data) => localStorage.setItem(key, JSON.stringify(data));
+
+// Math.random should be unique because of its seeding algorithm.
+// Convert it to base 36 (numbers + letters), and grab the first 9 characters
+// after the decimal.
+export const genId = () => `_${Math.random().toString(36).substr(2, 9)}`;
